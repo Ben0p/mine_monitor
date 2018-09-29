78fc45f66563b9b64f5ac8d2f7319ca6cd49e034
Benjamin Gorham <bgorham@fmgl.com.au>
2018-09-29 16:20:45 +0800

    Get all data via modbus, get latency.

--- backend/mm_signs.py
+++ backend/mm_signs.py
@@ -6,184 +6,206 @@ import copy
 import time
 import json
 from pyModbusTCP.client import ModbusClient
+import subprocess, platform
+import os
 
 '''
 Mine systems status board backend status polling script thing
 if youAreReadingThis:
-    raw_input("How bored are you?")
-
-Reminder to self,
-Dictionaries and keys are UPPERCASE
-Strings use double quotes ""
-Functions are camelCase
-
+    input("How bored are you?")
 '''
 
-#Adam I/O controller login
-user = 'root'
-passw = '00000000'
 
-#Linux box .xml location
-signs_file = '/home/minesys/Desktop/signs.xml'
+def getDetails(filein):
+    '''
+    Parses signs_file, returns list of dictionaries with sign information.
+    '''
 
-#Blank array and DICT
-signs = []
-SIGN = {}
+    # Reset the array
+    signs = []
 
-
-def getIPs (xmlfile):
     # Parse the .xml
-    tree = ElementTree.parse(xmlfile)
+    tree = ElementTree.parse(filein)
     # Get root of the xml
     root = tree.getroot()
-    # Extract location and ip
+
+    # Extract location, ip and type
     for child in root:
+        # Reset the SIGN dictioary
+        SIGN = {
+            'LOCATION' : '',
+            'IP' : '',
+            'TYPE' : '',
+            'ONLINE' : False,
+            'LATENCY' : 999, 
+            'SIGN' : False,
+            'CALERT' : False,
+            'TRAILER' : False,
+            'REST' : False,
+            'ALL_CLEAR' : False,
+            'EMERGENCY' : False,
+            'LIGHTNING' : False,
+            'A' : False,
+            'B' : False,
+            'C' : False
+        }
         SIGN['LOCATION'] = child[1].text
         SIGN['IP'] = child[3].text
         # Determine if 'C' Alert, Trailer or Sign
         alert = SIGN['LOCATION'].split('-')
         if len(alert) == 2:
             if alert[1] == ' C Alert':
-                SIGN['SETUP'] = 'C Alert'
+                SIGN['TYPE'] = 'C Alert'
                 SIGN['CALERT'] = True
-                SIGN['SIGN'] = False
-                SIGN['TRAILER'] = False
             elif alert[0][:3] == 'TR0':
-                SIGN['SETUP'] = 'Trailer'
-                SIGN['CALERT'] = False
-                SIGN['SIGN'] = False
+                SIGN['TYPE'] = 'Trailer'
                 SIGN['TRAILER'] = True
             else:
-                SIGN['SETUP'] = 'sign'
-                SIGN['CALERT'] = False
+                SIGN['TYPE'] = 'Sign'
                 SIGN['SIGN'] = True
-                SIGN['TRAILER'] = False
         else:
-            SIGN['SETUP'] = 'Sign'
-            SIGN['CALERT'] = False
+            SIGN['TYPE'] = 'Sign'
             SIGN['SIGN'] = True
-            SIGN['TRAILER'] = False
-
 
         # Duplicate the dictionary
         my_copy = copy.deepcopy(SIGN)
         # Append to list
         signs.append(my_copy)
+    return(signs)
 
+def ping(host):
+    """
+    Returns True and latency if host responds to a ping request
+    """
+    if platform.system().lower()=="windows":
 
-
-def getType (ip):
-    DO_XML = 'http://{}/digitaloutput/all/value'.format(ip)
-    with requests.Session() as session:
         try:
-            response = session.get(DO_XML, auth=(user, passw), timeout=1)
-            status = response.status_code
-            if response.status_code == 200:
-                sign_type = 'REST API'
-            elif response.status_code == 404:
-                sign_type = 'MODBUS'
-        except:
-            status = 'TIMEOUT'
-            sign_type = 'OFFLINE'
-    return(status, sign_type)
+            response = subprocess.check_output(
+                ['ping', '-n', '1', host],
+                stderr=subprocess.STDOUT,  # get all output
+                universal_newlines=True,  # return string not bytes
+                shell=False
+            )
+            latencies = response.splitlines()[7]
+            latency = latencies.split('=')[3][1]
+            result = True
+        except subprocess.CalledProcessError:
+            latency = 999
+            result = False
+    else:
+        try:
+            response = subprocess.check_output(
+                ['ping', '-c', '1', host]
+            )
+            latencies = response.splitlines()[5]
+            latency = latencies.decode().split('/')[4][0]
+            result = True
+        except subprocess.CalledProcessError:
+            latency = 999
+            result = False
+
+    return(result, latency)
+
+
 
 
 def restAPI(ip):
-    DO_XML = 'http://{}/digitaloutput/all/value'.format(ip)
+    '''
+    Checks for active Rest API on supplied ip
+    '''
+
+    #Adam I/O controller login
+    user = 'root'
+    passw = '00000000'
+    
+    do_xml = 'http://{}/digitaloutput/all/value'.format(ip)
     with requests.Session() as session:
         try:
-            response = session.get(DO_XML, auth=(user, passw), timeout=1)
-            status = response.status_code
+            response = session.get(do_xml, auth=(user, passw), timeout=1)
             tree = ElementTree.fromstring(response.content)
-            all_clear = tree[0][1].text
-            emergency = tree[1][1].text
-            lightning = tree[2][1].text
-            a = tree[3][1].text
-            b = tree[4][1].text
-            c = tree[5][1].text
+            return(True)
         except:
-            status = 'TIMEOUT'
-            all_clear = ''
-            emergency = ''
-            lightning = ''
-            a = ''
-            b = ''
-            c = ''
-    outputs = [all_clear, emergency, lightning, a, b, c]
-    return(status, outputs)
+            return(False)
 
 
 def modbus(ip):
-    outputs = []
+    '''
+    Get output states via modbus over ip.
+    '''
+    # Setup modbus connection
     c = ModbusClient(host=ip, port=502, auto_open=True)
+    # Read Modbus outputs 
     bits = c.read_coils(16, 6)
+    # If there is a response
     if bits:
-        status = 'OK'
-        for bit in bits:
-            if bit == True:
-                outputs.append(1)
-            elif bit == False:
-                outputs.append(0)
+        # Set output states
+        return([bits[0], bits[1], bits[2], bits[3], bits[4], bits[5]])
     else:
-        status = 'TIMEOUT'
-        outputs = ['','','','','','']
-    return(status, outputs)
+        return(['', '', '', '', '', ''])
 
 
-def saveJSON(signs):
+def saveJSON(signs, fileout):
+    '''
+    Converts list of sign dictionaries to json
+    '''
     signs_json = json.dumps(signs)
-    with open('/usr/share/nginx/html/assets/json/signs.json', 'w') as f:
+    with open(fileout, 'w') as f:
         f.write(signs_json)
         f.close()
 
 
+def getAll(filein, fileout):
+    '''
+    '''
+
+    # Generate array of signs with location and IP
+    signs = getDetails(filein)
+
+    # For each sign...
+    for sign in signs:
+        # Check if online
+        sign['ONLINE'] = ping(sign['IP'])[0]
+        sign['LATENCY'] = ping(sign['IP'])[1]
+        if sign['ONLINE']:
+            # Check if REST API available
+            sign['REST'] = restAPI(sign['IP'])
+            # Get modbus outputs
+            outputs = modbus(sign['IP'])
+            # Set output states
+            sign['ALL_CLEAR'] = outputs[0]
+            sign['EMERGENCY'] = outputs[1]
+            sign['LIGHTNING'] = outputs[2]
+            sign['A'] = outputs[3]
+            sign['B'] = outputs[4]
+            sign['C'] = outputs[5]
+        # Print results
+        print('{}'.format(sign['LOCATION']))
+        print('--IP: {}'.format(sign['IP']))
+        print('--Type: {}'.format(sign['TYPE']))
+        print('--Online: {}'.format(sign['ONLINE']))
+        print('--Latency: {}ms'.format(sign['LATENCY']))
+        print('---------------')
+    
+    # Convert to json and save to file
+    saveJSON(signs, fileout)
+
+    print('Sleeping for 30 sec...')
+    time.sleep(30)
+
+
+
 
 if __name__ == '__main__':
+
+    if platform.system().lower()=="windows":
+        # Testing file location
+        signs_file = './signs.xml'
+        json_file = '../../frontend/src/assets/json/signs.json'
+    else:
+        # Linux productin file locations
+        signs_file = '/home/minesys/Desktop/signs.xml'
+        json_file = '/usr/share/nginx/html/assets/json/signs.json'
+
+
     while True:
-        # Reset the dictionary and list
-        signs = []
-        SIGN = {}
-
-        # Append dictionary of name and IP to signs list
-        getIPs(signs_file)
-
-        # Determine if to use RESTful API or MODBUS and append to signs lits
-        for sign in signs:
-            # Get status and type
-            sign_type = getType(sign['IP'])
-            sign['TYPE'] = sign_type[1]
-            sign['STATUS'] = sign_type[0]
-            # Get output states
-            if sign_type[1] == 'OFFLINE':
-                sign['STATUS'] = 'TIMEOUT'
-                sign['ALL_CLEAR'] = ''
-                sign['EMERGENCY'] = ''
-                sign['LIGHTNING'] = ''
-                sign['A'] = ''
-                sign['B'] = ''
-                sign['C'] = ''
-            else:
-                if sign_type[1] == 'REST API':
-                    outputs = restAPI(sign['IP'])
-                elif sign_type[1] == 'MODBUS':
-                    outputs = modbus(sign['IP'])
-                sign['STATUS'] = outputs[0]
-                sign['ALL_CLEAR'] = outputs[1][0]
-                sign['EMERGENCY'] = outputs[1][1]
-                sign['LIGHTNING'] = outputs[1][2]
-                sign['A'] = outputs[1][3]
-                sign['B'] = outputs[1][4]
-                sign['C'] = outputs[1][5]
-
-            print('{}'.format(sign['LOCATION']))
-            print('--{}'.format(sign['IP']))
-            print('--{}'.format(sign['SETUP']))
-            print('--{}'.format(sign['STATUS']))
-            print('--{}'.format(sign['TYPE']))
-            print('---------------')
-        
-        # Convert to json and save to file
-        saveJSON(signs)
-
-        print('Sleeping for 30 sec...')
-        time.sleep(30)
+        getAll(signs_file, json_file)
